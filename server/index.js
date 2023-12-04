9f0ad82c5a9947b911eaf00315e24a246b77c403
Chelsea Schmidt <cschmidt331@gmail.com>
2023-12-04 13:23:31 -0800

    Make miscellaneous code organization and readability improvements

--- server/index.js
+++ server/index.js
@@ -2,14 +2,14 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const { PORT, HOME_URL } = require('./constants');
+
+const isProd = process.env.MODE === 'prod';
+const startupMessage = `Listening at ${HOME_URL} in ${isProd ? 'production' : 'development'} mode`;
 
 const server = express();
-const port = process.env.PORT || 3000;
-const isProd = process.env.MODE === 'prod';
-const publicDir = path.resolve(__dirname, '..', 'client', 'public');
-
-console.log({ port, isProd, publicDir });
 
 server.use(morgan(isProd ? 'tiny' : 'dev'));
-server.use(express.static(publicDir));
-server.listen(port, () => console.log(`Listening at http://localhost:${port}`));
+server.use(express.static(path.resolve(__dirname, '..', 'client', 'public')));
+
+server.listen(PORT, () => console.log(startupMessage));
